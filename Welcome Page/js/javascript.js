d37632fba4a7331a10d0f81dfb2638742f3d1fdd
Aniello Scartozzi <ascartoz@hawk.iit.edu>
2017-11-30 17:13:39 -0600

    fixed javascript in html page

--- Welcome Page/js/javascript.js
+++ Welcome Page/js/javascript.js
@@ -1,15 +1,3 @@
-function initMap() {
-    var iit = {lat: 41.8349, lng: -87.6270};
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 10,
-      center: iit
-    });
-    var marker = new google.maps.Marker({
-      position: iit,
-      map: map
-    });
-  }
-google.maps.event.addDomListener(window, 'load', initialize);
 
 function mobileNav() {
     var x = document.getElementById("myTopnav");
