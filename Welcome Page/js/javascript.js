dc4908a5db664a84972c014cc0607f5c2d339fae
snayyer <snayyer@hawk.iit.edu>
2017-12-05 13:22:35 -0600

    Add pages to the main directory

--- Welcome Page/js/javascript.js
+++
@@ -1,9 +0,0 @@
-
-function mobileNav() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
